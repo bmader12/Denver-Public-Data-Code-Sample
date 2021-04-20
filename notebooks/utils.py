8a2b9ed45d10e7520db84e8758ee65b45fc0610f
Brian Mader <brian.e.mader@gmail.com>
2021-04-20 14:52:06 -0600

    Update utils.py

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -8,7 +8,11 @@ import numpy as np
 import seaborn as sns
 
 
-
+def read_remote_csv(zip_file_url: str, file_name: str) -> pd.DataFrame:
+    remote_zip_file = urlopen(zip_file_url)
+    zip_mem = BytesIO(remote_zip_file.read())
+    zip_file = zipfile.ZipFile(zip_mem)
+    return pd.read_csv(zip_file.open(file_name), encoding="ISO-8859-1", dtype=str)
 
 def clean_accidents_df(accidents_df: pd.DataFrame) -> pd.DataFrame:
     """
